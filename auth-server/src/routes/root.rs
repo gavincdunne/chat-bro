1a437e9b2bc99c52e165b60f00fa8b89ab7635b3
gavincdunne <gavincdunne@gmail.com>
2025-06-04 12:19:18 -0400

    moving auth-server to own repo

--- auth-server/src/routes/root.rs
+++
@@ -1,5 +0,0 @@
-use axum::response::IntoResponse;
-
-pub async fn root_handler() -> impl IntoResponse {
-    "Hello from Rust Auth Server!"
-}
--- auth-server/src/state.rs
+++
@@ -1,4 +0,0 @@
-#[derive(Clone)]
-pub struct AppState {
-    pub db: sqlx::SqlitePool,
-}
